ada4e57b1bb63822cfc317a0190a29d742ba41d5
bkoimett <benkoimet@gmail.com>
2025-10-04 12:34:17 +0300

    MVP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import KenyaMap from "./components/KenyaMap";
+import { fetchBlooms } from "./services/api";
+import NavBar from "./components/NavBar";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchBlooms().then(setData);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="p-4">
+      <NavBar/>
+      {/* <h1 className="text-3xl font-bold mb-4">🌍 Bloom Watchers - Kenya</h1> */}
+      <KenyaMap data={data} />
+      <div className="mt-6 flex gap-2">
+        <button className="btn btn-primary">Filter</button>
+        <button className="btn btn-secondary">Predict</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
